ee22ba791b4c22d28c71d699a27c4a3fcaa67b06
Philippe Recto <precto1285@gmal.com>
2018-03-06 23:06:29 -0800

    Added Js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,34 @@
 //global variables:
-var names = ["Philippe", "Michael", "Dusty", "Danielle", "Tanya", "Dawn", "David", "Alan", "Delia"];
-var underScores;
+var names = ["Philippe", "Michael", "Dusty", "Danielle", "Tanya", "Dawn", "David", "Alan", "Delia", "Rene", "Nick", "Yano", "MaryAnne", "Franz", "Rich", "Trixie", "Vic", "Martin", "Jojie", "Susan", "George"];
 var winCount = 0;
 var LossCount = 0;
-var guessesLeft = 9;
+var guessesLeft = 10;
+var computerPick = "";
+var underScores = "";
 var userGuess;
-var wrongLetter ="";
+var result = "";
+var wrongLetter = "";
 
 
+
+function startGame(){
+
+delete compPick;
+delete underScores;
+delete userGuess;
+guessesLeft = 10;
+
+computerPick = names[Math.floor(Math.random() * names.length)];
+    console.log(computerPick);
+
+result = computerPick.split("");
+console.log(result);
+for (var i = 0; i < result.length; i++){
+    underScores += "_ ";
+    document.getElementById("word-blanks").innerHTML = underScores;
+}
+
+}
+
+
+startGame();
