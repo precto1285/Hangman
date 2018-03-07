649ca29f5b98032683653a02af3bea38dd6bdadf
Philippe Recto <precto1285@gmal.com>
2018-03-06 22:44:37 -0800

    this is my new attempt at Hangman Javascript

---
+++ assets/javascript/game.js
@@ -0,0 +1,10 @@
+//global variables:
+var names = ["Philippe", "Michael", "Dusty", "Danielle", "Tanya", "Dawn", "David", "Alan", "Delia"];
+var underScores;
+var winCount = 0;
+var LossCount = 0;
+var guessesLeft = 9;
+var userGuess;
+var wrongLetter ="";
+
+
